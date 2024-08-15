6df225798877cfc29b6d2f9e1ca420002931440f
David Borun <dborun@mail.ru>
2024-08-15 17:02:56 +0300

    Created flag_parser func

---
+++ src/cat/parser/flag_parser.c
@@ -0,0 +1,49 @@
+#include "flag_parser.h"
+
+CatFlags cat_parse_flags(int argc, char *argv[]) {
+  struct option longOptions[] = {{"number-nonblank", 0, NULL, 'b'},
+                                 {"number", 0, NULL, 'n'},
+                                 {"squeeze-blank", 0, NULL, 's'},
+                                 {NULL, 0, NULL, 0}};
+
+  CatFlags flags = {false};
+
+  int current_flag = 0;
+
+  while ((current_flag =
+              getopt_long(argc, argv, "benstvET", longOptions, NULL)) != -1) {
+    switch (current_flag) {
+      case 'b':
+        flags.numNonBlank = true;
+        break;
+      case 'e':
+        flags.symbolEndl = true;
+        flags.printNonPrintable = true;
+        break;
+      case 'v':
+        flags.printNonPrintable = true;
+        break;
+      case 'E':
+        flags.symbolEndl = true;
+        break;
+      case 'n':
+        flags.numAll = true;
+        break;
+      case 's':
+        flags.squeezeBlank = true;
+        break;
+      case 't':
+        flags.tab = true;
+        flags.printNonPrintable = true;
+        break;
+      case 'T':
+        flags.tab = true;
+        break;
+      case '?':  // неизвестный флаг
+        fprintf(stderr, "usage: s21_cat [-benstvET] [file ...]\n");
+        exit(EXIT_FAILURE);
+    }
+  }
+  
+  return flags;
+}
---
+++ src/cat/parser/flag_parser.h
@@ -0,0 +1,8 @@
+#ifndef FLAG_PARSER_H
+#define FLAG_PARSER_H
+
+#include "../../common/flag_struct.h"
+
+CatFlags cat_parse_flags(int argc, char *argv[]);
+
+#endif
