c01b5163ab21a7e6eed73344e11d4426fe3b282e
David Borun <dborun@mail.ru>
2024-08-15 17:03:56 +0300

    Based program logic

---
+++ src/cat/main.c
@@ -0,0 +1,15 @@
+#include "../common/flag_struct.h"
+#include "flags/cat_flags.h"
+#include "controller/cat_main_controller.h"
+#include "parser/flag_parser.h"
+
+// argc - счетчик количества параметров командой строки argc.
+// argv - значения парамеров командной строки argv[].
+
+int main(int argc, char *argv[]) {
+  CatFlags flags = cat_parse_flags(argc, argv);
+  const char *table[256];
+  cat_set_table(table);
+  cat(argc, argv, flags, table);
+  return 0;
+}
